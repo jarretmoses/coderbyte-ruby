173c187e3960d214e034f1be3a5d2a417e6d200c
Jarret Moses <jarret.moses@gmail.com>
2015-01-08 15:26:36 -0500

    letter_changes solved

---
+++ easy_challenges/letter_changes.rb
@@ -0,0 +1,18 @@
+def LetterChanges(str)
+  lower = ('a'..'z').to_a.join + 'a'
+  upper = ('A'..'Z').to_a.join + 'A'
+
+  str.each_char.with_index do |char,i|
+    if new_i = get_index(lower,char)
+      str[i] = lower[new_i]
+    elsif new_i = get_index(upper,char)
+      str[i] = upper[new_i]
+    end
+  end
+  str.gsub(/([aeiou])/i){|v| v.upcase}
+end
+
+def get_index(alph,letter)
+  alph.index(letter)+1 if alph.index(letter)
+end
+
